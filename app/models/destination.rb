8c3a082310999c07cf04c4140328abda68794557
jesustr20 <jesus.tr61@gmail.com>
2023-02-19 22:42:16 -0500

    modificacion en: rutas, controllers, messaes models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -18,6 +18,27 @@
 #  updated_at       :datetime         not null
 #
 class Destination < ApplicationRecord
+  include ValidatableEnum
+  #constants
+  ONLY_ALLOWS_LETTERS = { with: /\A[a-zA-Z]+\z/, message: "Solo se permiten letras"}
+
   #Associations
   belongs_to :journey
+  #Validations
+  validates :destination_name, presence: true
+  validates :firstname, presence: true, format: ONLY_ALLOWS_LETTERS
+  validates :lastname, presence: true, format: ONLY_ALLOWS_LETTERS
+  validates :dni, presence: true
+  validates :country, presence: true, format: ONLY_ALLOWS_LETTERS
+  validates :province, presence: true, format: ONLY_ALLOWS_LETTERS
+  validates :district, presence: true, format: ONLY_ALLOWS_LETTERS
+  validates :avenue, presence: true
+  validates :product_name, presence: true
+  #enum type_operation: {entrega: "entrega", retiro: "retiro"}
+  enum type_operation: %i[entrega retiro]  
+  validates :type_operation, inclusion: { 
+            in: type_operations.keys,
+            message: "Valores permitidos: #{type_operations.keys.join(', ')}}"
+            }
+  validatable_enum :type_operation
 end
